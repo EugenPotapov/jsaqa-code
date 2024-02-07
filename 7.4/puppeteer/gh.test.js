45585c37384bf0bfb544644683b268e2a41ab377
EugenPotapov <132608815+EugenPotapov@users.noreply.github.com>
2024-02-07 11:35:15 +0300

    HW7.4

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,25 +9,55 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  }, 10000);
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual(
+      "GitHub for teams · Build like the best teams on the planet · GitHub"
+    );
+  }, 10000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 10000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain(" Get started with Team");
+  }, 10000);
+});
+
+describe("Github page tests2", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/features");
+  }, 10000);
+
+  test("title", async () => {
+    const title = await page.title();
+    expect(title).toContain("Features | GitHub · GitHub");
+  }, 10000);
+
+  test("h1", async () => {
+    const expected =
+      "Search code, repositories, users, issues, pull requests...";
+    const actual = await page.$eval("h1", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 10000);
+
+  test("h2", async () => {
+    const expected = "Use saved searches to filter your results more quickly";
+    const actual = await page.$eval("h2", (link) => link.textContent);
+    expect(actual).toContain(expected);
+  }, 10000);
 });
